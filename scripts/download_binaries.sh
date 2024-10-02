e5ce7d4cdcdedc20d907bdce95770c45ecfaebf5
stossy11 <69031796+stossy11@users.noreply.github.com>
2024-10-02 10:51:54 +1000

    Update download_binaries.sh

--- scripts/download_binaries.sh
+++ scripts/download_binaries.sh
@@ -7,7 +7,7 @@ cd downloaded_binaries
 # 	wget https://download.visualstudio.microsoft.com/download/pr/58c27f9f-f988-4a42-be1a-0747657952f0/32c855c8c0ff149e4b3662ff3bc3e632/dotnet-sdk-7.0.101-osx-x64.tar.gz
 # fi
 wget https://download.visualstudio.microsoft.com/download/pr/d9df94f7-3ea2-41b6-abde-dcb9caa87056/9df759093dcdbc1a1b98feede2da8aaa/dotnet-sdk-7.0.101-osx-arm64.tar.gz
-wget https://github.com/Ryujinx/release-channel-macos/releases/download/1.1.0-macos1/Ryujinx-1.1.0-macos1-macos_universal.app.tar.gz
+wget https://archive.org/download/ryujinx-1.1.1401-macos_universal.app/ryujinx-1.1.1401-macos_universal.app.tar
 wget https://github.com/zhuowei/Ryujinx/releases/download/v0.0.1/ryujinx_arm64_build.tar.xz
 cd ..
 mkdir dotnetarm64 binaryrel
